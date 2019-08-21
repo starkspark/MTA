4ad4f4c6bccf276933753cf30b10429b56c8bd9a
Sierra <sierra.swaby@gmail.com>
2019-08-21 14:38:12 -0400

    db

---
+++ database/db.js
@@ -0,0 +1,17 @@
+const pg = require('pg');
+const uri = 'postgres://zluoskut:2wXOgERV2hFxySyiNXmkaUfSRYNcXZ64@isilo.db.elephantsql.com:5432/zluoskut';
+const pool = new pg.Pool({connectionString: uri});
+
+pool.connect((err, db) => {
+  if (err) console.log(err)
+  console.log("I'm here!")
+  
+  db.query('SELECT * FROM a', (err, res) => {
+    if (err) console.log(err);
+    console.log("in the query")
+    console.log(res)
+  })
+  // db.end();
+})
+
+module.exports = pool;
